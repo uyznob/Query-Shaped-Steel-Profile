377a74c7b97623fa2d07f95e10c3f234e8d30ebd
uyznob <uyznob@gmail.com>
2020-09-20 21:43:37 +0200

    Upload

---
+++ app/src/main/java/bttd/enginit/bttd/MainActivity.java
@@ -0,0 +1,37 @@
+package bttd.enginit.bttd;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: Starting...");
+
+        // Set up the ViewPager with the sections adapter
+        mViewPager = findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CalFrag(),"TÍNH TOÁN");
+        adapter.addFragment(new SearchFrag(), "TRA BẢNG");
+        adapter.addFragment(new NoteFrag(), "CHÚ THÍCH");
+
+        viewPager.setAdapter(adapter);
+    }
+}
